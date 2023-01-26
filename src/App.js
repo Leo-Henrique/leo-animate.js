af16a9df96af177c547c7073f0e1476523544f00
Leonardo <leonardo0507.henrique@gmail.com>
2023-01-26 00:53:42 -0300

    Created home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./scss/style.scss";
+import IMGJavaScript from "./assets/javascript.svg";
+import SVGGitHub from "./assets/svg-github";
 
 export default function App() {
-    return /*html*/`
-        <p>Hello World!</p>
+    return /*html*/ `
+        <div class="app__icon">
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="external noopener noreferrer">
+                <img src=${IMGJavaScript}
+                alt="JavaScript logo" />
+            </a>
+        </div>
+
+        <h1 class="app__title"
+        translate="no">
+            leo-animate.js
+        </h1>
+
+        <a class="btn"
+        href="https://github.com/Leo-Henrique/leo-react-app"
+        target="_blank">
+            <span>${SVGGitHub()}</span>
+
+            <span>Get Started</span>
+        </a>
     `;
 }
