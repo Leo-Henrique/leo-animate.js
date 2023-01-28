b4e3e5684f9c8df6f62b60715f390ca3ffa383f0
Leonardo <leonardo0507.henrique@gmail.com>
2023-01-27 23:28:25 -0300

    Part SASS of the created library. Updated folder structure and added animations in home page

--- demo/App.js
+++ demo/App.js
@@ -8,7 +8,8 @@ export default function App() {
 
     return /*html*/ `
         <div class="app__icon"
-        data-animate>
+        data-animate="fadeDown"
+        data-animate-md="fadeDown">
             <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
             target="_blank"
             rel="external noopener noreferrer">
@@ -19,12 +20,14 @@ export default function App() {
 
         <h1 class="app__title"
         translate="no"
-        data-animate="fadeRight">
+        data-animate="fadeRight"
+        data-animate-md="fadeDown">
             leo-animate.js
         </h1>
 
         <div class="btn"
-        data-animate="fadeUp">
+        data-animate="fadeUp"
+        data-animate-md="fadeDown">
             <a class="btn__anchor"
             href="https://github.com/Leo-Henrique/leo-react-app"
             target="_blank">
